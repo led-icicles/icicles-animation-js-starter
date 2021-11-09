85c8ede255c375f0fae1b98b31ce5c712b599e1d
gonuit <kamilklyta341@gmail.com>
2021-11-09 23:07:19 +0100

    Add example animation

---
+++ src/index.ts
@@ -0,0 +1,53 @@
+import { Animation, Color, Colors, Duration, Icicles } from "icicles-animation";
+
+// Define function that will compile our animation
+const compile = async () => {
+  // Define icicles size
+  const iciclesCount = 20;
+  const ledsPerIcicle = 30;
+
+  // Helper class for creating animation frames.
+  const icicles = new Icicles(iciclesCount, ledsPerIcicle);
+  // Animation file representation
+  const animation = new Animation({
+    name: "Przykładowa animacja",
+    optimize: true,
+    versionNumber: 1,
+    xCount: iciclesCount,
+    yCount: ledsPerIcicle,
+    loopsCount: 5,
+  });
+
+  // Create a function that will set icicles color from top to bottom
+  const animateColorFromTopToBottom = (color: Color) => {
+    for (let rowIndex = 0; rowIndex < ledsPerIcicle; rowIndex++) {
+      // Set icicles row to provided color
+      icicles.setRowColor(rowIndex, color);
+      // Define frame duration
+      const duration = new Duration({ milliseconds: 32 });
+      // Get current icicles state as animation frame
+      const frame = icicles.toFrame(duration);
+      // Add a frame to the animation
+      animation.addFrame(frame);
+    }
+  };
+
+  // Run defined function for multiple colors
+  animateColorFromTopToBottom(Colors.green);
+  animateColorFromTopToBottom(Colors.blue);
+  animateColorFromTopToBottom(Colors.magenta);
+  animateColorFromTopToBottom(Colors.yellow);
+  animateColorFromTopToBottom(Colors.violet);
+  animateColorFromTopToBottom(Colors.red);
+  animateColorFromTopToBottom(Colors.orange);
+  animateColorFromTopToBottom(Colors.oceanBlue);
+  animateColorFromTopToBottom(Colors.lawnGreen);
+
+  // Save the created animation to the file under the given path.
+  await animation.toFile(`compiled/example.anim`);
+
+  // GREAT! Now you can play the animation on https://icicles.intror.com/
+};
+
+// Compile animation
+compile();
